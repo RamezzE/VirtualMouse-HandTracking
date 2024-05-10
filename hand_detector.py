665b99d5287edc800d9edbdca111554d3d324771
Ramez Ehab <ramezehab2@gmail.com>
2024-05-10 13:40:09 +0300

    Moving mouse when only index is up

--- hand_detector.py
+++ hand_detector.py
@@ -1,36 +1,91 @@
 import mediapipe as mp
+from cvzone.HandTrackingModule import HandDetector
 import cv2
+import pyautogui
 
 class Hand_Detector:
-    LEFT_HAND = 0
-    RIGHT_HAND = 1
-
+    screen_width, screen_height = pyautogui.size()
+    
+    THUMB = 4
+    INDEX = 8
+    MIDDLE = 12
+    RING = 16
+    PINKY = 20
+    
     # Constructor
     def __init__(self):
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands()
+        self.mpHandsDetector = self.mpHands.Hands()
         self.mpDraw = mp.solutions.drawing_utils
         
-    def process(self, img):
+        self.detector = HandDetector(detectionCon = 0.8)
+        
+    def findHands(self, img, drawConnections = True, drawAll = True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.img_height, self.img_width, _ = img.shape
-        self.results = self.hands.process(imgRGB)
-
-    def drawHands(self, img, fingerIdsToDraw = [], drawConnections = True):
-        hands = self.results.multi_hand_landmarks
-        if not hands:
+        self.results = self.mpHandsDetector.process(imgRGB)
+        self.hands = self.results.multi_hand_landmarks
+        
+        self.CV_hands, img = self.detector.findHands(img, drawAll)
+        
+        if drawAll:
             return img
         
-        for hand in hands:
-            if drawConnections:
+        if drawConnections:
+            if not self.hands:
+                return img
+            
+            for hand in self.hands:
                 self.mpDraw.draw_landmarks(img, hand, self.mpHands.HAND_CONNECTIONS)
-                
-            landmarks = hand.landmark
-                
-            for id, landmark in enumerate(landmarks):
-                if id in fingerIdsToDraw:
-                    x = int(landmark.x * self.img_width)
-                    y = int(landmark.y * self.img_height)
-                    cv2.circle(img = img, center = (x, y), radius = 10, color = (255, 0, 0), thickness = cv2.FILLED)
-                            
+            
         return img
+    
+    
+    def highlightFingers(self, img, fingers = [THUMB, INDEX, MIDDLE, RING, PINKY]):
+        if not self.hands:
+            return img
+        
+        for hand in self.hands:
+            landmarks = hand.landmark
+      
+            for id, landmark in enumerate(landmarks):
+                if id not in fingers:
+                    continue
+                
+                x = int(landmark.x * self.img_width)
+                y = int(landmark.y * self.img_height)
+                cv2.circle(img = img, center = (x, y), radius = 10, color = (255, 0, 0), thickness = cv2.FILLED)
+                
+        return img
+    
+    def getFingerPosition(self, fingerId, hand = 0):
+        landmarks = self.getLandmarks(hand)
+        if landmarks and fingerId < len(landmarks):
+            finger = landmarks[fingerId]
+            x = int(finger.x * self.img_width)
+            y = int(finger.y * self.img_height)
+            
+            x = int(self.screen_width / self.img_width * x)
+            y = int(self.screen_height / self.img_height * y)
+            return x, y 
+        return None, None
+
+    
+    def getLandmarks(self, hand = 0):
+        if self.hands and hand < len(self.hands):
+            return self.hands[hand].landmark
+        return None
+    
+    def getFingersUp(self, hand = 0):
+        if not self.CV_hands or hand >= len(self.CV_hands):
+            return [0, 0, 0, 0, 0]
+        
+        return self.detector.fingersUp(self.CV_hands[hand])
+    
+    def isFingerOnlyUp(self, finger, hand = 0):
+        fingers = self.getFingersUp(hand)
+        if not fingers:
+            return False
+        
+        return sum (fingers) == 1 and fingers[int(finger / 4) -1] == 1
+    
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import cv2
 from hand_detector import Hand_Detector
+from mouse_controller import Mouse_Controller
+import pyautogui
 
 HD = Hand_Detector()
+MC = Mouse_Controller(pyautogui.size())
 
 cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
     
-    HD.process(frame)
-    
-    frame = HD.drawHands(img = frame, fingerIdsToDraw = [4,8,20], drawConnections = True)
+    frame = cv2.flip(frame, 1)
+        
+    frame = HD.findHands(img = frame, drawConnections = True, drawAll = False)
+    frame = HD.highlightFingers(img = frame, fingers = [HD.THUMB, HD.INDEX, HD.MIDDLE])
+
+    if HD.isFingerOnlyUp(HD.INDEX):
+        MC.moveMouse(pos = HD.getFingerPosition(HD.INDEX), sensitivity = 1.5)
     
     cv2.imshow('Virtual Mouse', frame)
 
