6d6176aa613889bb7d7b521b8c15c8f18ebe60e4
Ramez Ehab <ramezehab2@gmail.com>
2024-05-12 20:27:52 +0300

    training and testing a tensorflow model for gesture classification

--- testing/live_testing.py
+++ testing/live_testing.py
@@ -1,14 +1,18 @@
 import cv2
 from handDetector import HandDetector
-import pandas as pd
+import tensorflow as tf
+import numpy as np
+
+model = tf.keras.models.load_model('model/model2.keras')
 
 HD = HandDetector()
 
-cap = cv2.VideoCapture(0)
+ 
+string_to_numeric = {'idle': 0, 'left_click': 1, 'right_click' : 2, 'move_mouse': 3, 'press_and_hold_left_click': 4, 'scroll': 5}
 
-collected_landmarks = []
-labels = []
-labelName = "idle_5"
+numeric_to_string = {v: k for k, v in string_to_numeric.items()}        
+
+cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
@@ -18,12 +22,15 @@ while True:
     frame = HD.findHands(img = frame, drawConnections = True)
             
     landmarks = HD.getLandmarks()
-    
-    frame_landmarks = []
-    
+            
     if landmarks:
+        # flatten the landmarks
+        landmarks_array = np.array([[lm.x, lm.y, lm.z] for lm in landmarks])
+        landmarks_array = landmarks_array.reshape(1, -1)
+        
         x_values = []
         y_values = []
+        X = []
         
         for landmark in landmarks:
             x_values.append(landmark.x)
@@ -41,28 +48,24 @@ while True:
             y_values_normalized = [(y - min_y) / (max_y - min_y) for y in y_values]
             
             for x, y in zip(x_values_normalized, y_values_normalized):
-                frame_landmarks.append(x)
-                frame_landmarks.append(y)
+                X.append(x)
+                X.append(y)
                 
-            collected_landmarks.append(frame_landmarks)
-            
-            labels.append(labelName)
+        X = np.array(X)
+        X = X.reshape(1, -1)
+        y_pred_probabilities = model(X)
+
+
+        y_pred_index = np.argmax(y_pred_probabilities, axis=1)
+        y_pred = numeric_to_string[y_pred_index[0]]
         
-        
-    cv2.imshow('Collecting Data', frame)
+        print(y_pred_index, y_pred, y_pred_probabilities)
+                    
+    cv2.imshow('Live Testing', frame)
     key = cv2.waitKey(1)
   
     if key == ord('q'):
         break
-    
-    
-X = pd.DataFrame(collected_landmarks)
-y = pd.DataFrame(labels)
-
-df = pd.concat([X, y], axis = 1)
-
-csvFile = f"training/Collected Dataset/{labelName}.csv"
-df.to_csv(csvFile, index = False)
 
 cap.release()
 cv2.destroyAllWindows()
