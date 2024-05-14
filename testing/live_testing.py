3ff2cdfc2c3a99ef5cee9a12cf0cd2152fce8aeb
Ramez Ehab <ramezehab2@gmail.com>
2024-05-15 01:57:28 +0300

    Trained and tested the model with more gestures

--- testing/live_testing.py
+++ testing/live_testing.py
@@ -5,14 +5,19 @@ import numpy as np
 
 model = tf.keras.models.load_model('model/model2.keras')
 
+string_to_numeric = {'idle': 0, 'left_click': 1, 'right_click' : 2, 'move_mouse': 3, 'press_and_hold_left': 4, 'pinch_action' : 5, 'scroll': 6, 'double_click':7}
+
+numeric_to_string = {value: key for key, value in string_to_numeric.items()}
+
 HD = HandDetector()
 
- 
-string_to_numeric = {'idle': 0, 'left_click': 1, 'right_click' : 2, 'move_mouse': 3, 'press_and_hold_left_click': 4, 'scroll': 5}
-
-numeric_to_string = {v: k for k, v in string_to_numeric.items()}        
-
 cap = cv2.VideoCapture(0)
+
+def preproceess(arr):
+    min_val = min(arr)
+    max_val = max(arr)
+    
+    return [(val - min_val) / (max_val - min_val) for val in arr]
 
 while True:
     _, frame = cap.read()
@@ -22,44 +27,40 @@ while True:
     frame = HD.findHands(img = frame, drawConnections = True)
             
     landmarks = HD.getLandmarks()
-            
-    if landmarks:
-        # flatten the landmarks
-        landmarks_array = np.array([[lm.x, lm.y, lm.z] for lm in landmarks])
-        landmarks_array = landmarks_array.reshape(1, -1)
+    
+    X = []
         
+    if landmarks:
         x_values = []
         y_values = []
-        X = []
+        z_values = []
         
         for landmark in landmarks:
             x_values.append(landmark.x)
             y_values.append(landmark.y)
+            z_values.append(landmark.z)
             
         # if not empty
-        if x_values != [] and y_values != []:
-            min_x = min(x_values)
-            max_x = max(x_values)
+        if x_values != [] and y_values != [] and z_values != []:
+            x_values_normalized = preproceess(x_values)
+            y_values_normalized = preproceess(y_values)
+            z_values_normalized = preproceess(z_values)
             
-            min_y = min(y_values)
-            max_y = max(y_values)
-
-            x_values_normalized = [(x - min_x) / (max_x - min_x) for x in x_values]
-            y_values_normalized = [(y - min_y) / (max_y - min_y) for y in y_values]
-            
-            for x, y in zip(x_values_normalized, y_values_normalized):
+            for x, y, z in zip(x_values_normalized, y_values_normalized, z_values_normalized):
                 X.append(x)
                 X.append(y)
+                X.append(z)
                 
+
+        X = [coord for coords in zip(x_values_normalized, y_values_normalized, z_values_normalized) for coord in coords]
         X = np.array(X)
         X = X.reshape(1, -1)
         y_pred_probabilities = model(X)
 
-
         y_pred_index = np.argmax(y_pred_probabilities, axis=1)
         y_pred = numeric_to_string[y_pred_index[0]]
         
-        print(y_pred_index, y_pred, y_pred_probabilities)
+        print(y_pred)
                     
     cv2.imshow('Live Testing', frame)
     key = cv2.waitKey(1)
